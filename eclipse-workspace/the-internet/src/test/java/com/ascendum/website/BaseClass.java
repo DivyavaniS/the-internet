be786eaed41064176f67a020f737783240d4cb6a
Divyavani S <divyasri213@gmail.com>
2019-06-19 23:38:11 -0500

    changes in base class

--- eclipse-workspace/the-internet/src/test/java/com/ascendum/website/BaseClass.java
+++ eclipse-workspace/the-internet/src/test/java/com/ascendum/website/BaseClass.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
+	// div
 	WebDriver driver;
 	String name="divya";
   @BeforeMethod
